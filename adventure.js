5b022b8c489d0dcb746d7f2a3692eb5c542b55b8
Gerben <99052865@mydavinci.nl>
2019-11-25 13:48:23 +0100

    added sprites.
    Added some item sprites and backgrounds.

--- adventure.js
+++ adventure.js
@@ -54,44 +54,46 @@ function bonus3() {
 	
 }
 
-const DEBUG = true;
+var speed = 15; //bepaald de snelhijd van het character.
+
+var box1 = []
+var box2 = []
+var boxes = [box1, box2]
 
 var myIndex = 0;
-var marioJump =  [-55, -215, -385];
-var marioWalk = [-55, -215];
+var marioJump =  [-55, -215, -385];		//posities van de sprites op spritesheet van de springanimatie
+var marioWalk = [-55, -215];	//posities van de sprites op spritesheet van de loopanimatie
 var mIndexWalkLeft = 0;		// bepaalt welke index van array marioWalk gebruikt moet worden voor .......
 var mIndexWalkRight = 0;
 var mIndexJump = 0; // bepaalt welke index van array marioJump gebruikt moet worden voor .......
-var mEven = false;	
-var mEvenLeft = false;
-var mEvenRight = false;
-var m = document.getElementById("mario"); // slaap html element met id mario op in variabel .. var wel goeie naam geven dat later herkenbaar wordt
-var totalJump = 0;
-var inventory = 'closed';
-var walking = 0;
-var jumping = false;
 
-function mario(){
-	jumping = true;
-	totalJump++;
-	if(totalJump == 10){
+var mEven = false;	
+var momentumLeft = 0;
+var momentumRight = 0;
+var marioHtml = document.getElementById("mario"); // slaap html element met id mario op in variabel .. var wel goeie naam geven dat later herkenbaar wordt
+var totalJump = 0;		//telt de aantal sprongen
+var inventory = 'closed';	//toggelt de inventory
+var walking = false;	//zorgt ervoor of er kan worden gelopen
+var jumping = false;	//zorgt ervoor of er gesprongen kan worden of niet
+
+if(totalJump == 10){
 		//document.body.style.backgroundColor = "red";
 		//veranderd de achtergrond kleur als er 10x word gesprongen.
 	}
 
+
+document.addEventListener("keypress", function(e) { 	
+
+if(e.keyCode == 13 && !jumping){
+	jumping = true;
+	totalJump++;
 	mIndexJump = 1;
 	var animation = setInterval(function(){
 	
-		m.style.backgroundPosition = marioJump[mIndexJump] + "px 0";
-		//console.log(marioJump[mIndex] + "px 0");
-		m.style.marginTop = marioJump[mIndexJump] / 2 + "px";
-
-		console.log("m marginTop" + m.style.marginTop);
-
-		if(DEBUG)
-		console.log("DEBUG FUNCTION MARIO :::: mEven :"+ mEven +"| mIndexJump :" + mIndexJump + " marioJump.length - 1 : "+ (marioJump.length - 1));
-
-		//console.log(m.style.marginTop + "px 0");
+		marioHtml.style.backgroundPosition = marioJump[mIndexJump] + "px 0";
+		marioHtml.style.marginTop = marioJump[mIndexJump] + "px";
+		marioHtml.style.marginLeft = Number(marioHtml.style.marginLeft.replace('px', '') ) - momentumLeft + 'px'; //beweegt het character naar links tijdens de sprong als het momentum heeft
+		marioHtml.style.marginLeft = Number(marioHtml.style.marginLeft.replace('px', '') ) + (momentumRight) + 'px'; //beweegt het character naar rechts tijdens de sprong als het momentum heeft
 		if(mIndexJump == marioJump.length - 1){
 			mEven = true;
 		} 
@@ -102,28 +104,22 @@ function mario(){
 		}
 		if(mIndexJump < 0){
 			mEven = false;
-			m.style.marginTop = "0px";
+			marioHtml.style.marginTop = "0px";
 			clearInterval(animation);
 		}
 	}, 100);
 	setTimeout(function(){jumping = false;}, 300);
+	//laat het character springen elke 300 ms.
 }
 
-
-document.addEventListener("keypress", function(e) { 	
-
-if(e.keyCode == 13 && !jumping && !walking){
-		mario();
-		//voert de functie uit die het character laat springen als "enter" ingedrukt word.
-	}
-
-if(e.key == 'a' && !walking && !jumping){
-	walking = 1;
-	m.style.transform = "scaleX(-1)";
+if(e.key == 'a' && !walking){
+	walking = true;
+	momentumLeft = 30;
+	marioHtml.style.transform = "scaleX(-1)";
 	mIndexWalkLeft = 1;
 	var animation = setInterval(function(){	
-		m.style.backgroundPosition = marioWalk[mIndexWalkLeft] + "px 0";
-		m.style.marginLeft = Number(m.style.marginLeft.replace('px', '') ) - 10 + 'px';
+		marioHtml.style.backgroundPosition = marioWalk[mIndexWalkLeft] + "px 0";
+		marioHtml.style.marginLeft = Number(marioHtml.style.marginLeft.replace('px', '') ) - speed + 'px';
 		if(mIndexWalkLeft == marioWalk.length - 1){
 			mEvenLeft = true;
 		} 
@@ -137,17 +133,19 @@ if(e.key == 'a' && !walking && !jumping){
 			clearInterval(animation);
 		}
 	}, 150);
-	setTimeout(function(){walking = 0;}, 300);
-	//laat het character naar links lopen.
+	setTimeout(function(){walking = false;}, 300);
+	setTimeout(function(){momentumLeft = 0;}, 1200); //help
+	//laat het character naar links lopen elke 300 ms.
 }
 
-if(e.key == 'd' && !walking && !jumping){
-	walking = 1;
-	m.style.transform = "scaleX(+1)";
+if(e.key == 'd' && !walking){
+	walking = true;
+	momentumRight = 30;
+	marioHtml.style.transform = "scaleX(+1)";
 	mIndexWalkRight = 1;
 	var animation = setInterval(function(){
-		m.style.backgroundPosition = marioWalk[mIndexWalkRight] + "px 0";
-		m.style.marginLeft = Number(m.style.marginLeft.replace('px', '') ) + 10 + 'px';
+		marioHtml.style.backgroundPosition = marioWalk[mIndexWalkRight] + "px 0";
+		marioHtml.style.marginLeft = Number(marioHtml.style.marginLeft.replace('px', '') ) + speed + 'px';
 		if(mIndexWalkRight == marioWalk.length - 1){
 			mEvenRight = true;
 		} 
@@ -161,10 +159,11 @@ if(e.key == 'd' && !walking && !jumping){
 			clearInterval(animation);
 		}
 	}, 150);
-	setTimeout(function(){walking = 0;}, 300);
-	//laat het character naar rechts lopen.
-}else if (e.key == 'd' && m.style.marginLeft >= "500px") {
-	
+	setTimeout(function(){walking = false;}, 300);
+	setTimeout(function(){momentumRight = 0;}, 1200); //help
+	//laat het character naar rechts lopen elke 300 ms.
+}else if (e.key == 'd' && marioHtml.style.marginLeft >= "500px") {
+	console.log("AAAAAAAAAA")
 }
 
 if (e.key == 'e' && inventory == 'closed'){
@@ -184,7 +183,7 @@ function getHalfPage(){
 	width = width / 2;
 	width = width + 150;
 	return width;
-
+	//vindt de helft van de breedte van de pagina
 }
 
 
@@ -193,8 +192,13 @@ if(1000 < getHalfPage()){
 } else {
 	
 }
-//alleen laten lopen als de knop ingedrukt blijft? 
-//achtergronden  && m.style.marginLeft <= "500px" 
+
+//overal waar 'help' achter staat, helpen.
+//achtergrond, bomen, de ondergrond.
+//achtergrond laten bewegen ipv character
 //obstakels
+//health
+//wolken
 //items in de inventory plaatsen
+//vijhanden
 //meerdere skins? (mijn idee)
